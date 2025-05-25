c13a5f9351baf2feb198f6664823a5b0dad953f8
AiAyush <130283246+AiAyush@users.noreply.github.com>
2025-05-25 10:44:23 +0530

    Intial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Bulding Chrome Plugin analyze YouTube comment sentiments',
+    author='Ayush Chandra Maurya',
+    license='MIT',
+)
